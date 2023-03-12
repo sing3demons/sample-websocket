5056cd1ae66dea764cf2f858b4dac6edb7f1907d
sing3demons <sing3demons@live.com>
2023-03-12 09:57:26 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sing3demons/sample-websocket
+
+go 1.20
+
+require github.com/gorilla/websocket v1.5.0
---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+type ChatServer struct {
+	clients map[*websocket.Conn]struct{}
+	mu      sync.Mutex
+}
+
+func (cs *ChatServer) AddClient(conn *websocket.Conn) {
+	cs.mu.Lock()
+	defer cs.mu.Unlock()
+	cs.clients[conn] = struct{}{}
+}
+
+func (cs *ChatServer) RemoveClient(conn *websocket.Conn) {
+	cs.mu.Lock()
+	defer cs.mu.Unlock()
+	delete(cs.clients, conn)
+}
+
+func (cs *ChatServer) Broadcast(msg []byte) {
+	cs.mu.Lock()
+	defer cs.mu.Unlock()
+
+	for conn := range cs.clients {
+		go func(conn *websocket.Conn) {
+			err := conn.WriteMessage(websocket.TextMessage, msg)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+		}(conn)
+	}
+}
+
+func main() {
+	var upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+
+	chatServer := &ChatServer{
+		clients: make(map[*websocket.Conn]struct{}),
+		mu:      sync.Mutex{},
+	}
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		defer conn.Close()
+
+		chatServer.AddClient(conn)
+		defer chatServer.RemoveClient(conn)
+		for {
+			_, msg, err := conn.ReadMessage()
+			if err != nil {
+				log.Println(err)
+				return
+			}
+
+			log.Println(string(msg))
+			chatServer.Broadcast(msg)
+			// err = conn.WriteMessage(websocket.TextMessage, msg)
+			// if err != nil {
+			// 	log.Println(err)
+			// 	return
+			// }
+		}
+
+	})
+
+	http.HandleFunc("/chat", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
